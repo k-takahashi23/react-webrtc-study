929f11e310963b8bb1555fbb9d7596fdde1bc050
k-takahashi23 <efl.sad23@gmail.com>
2021-05-09 23:17:39 +0900

    MOD: connect video using signaling server

--- client/src/components/Video.tsx
+++ client/src/components/Video.tsx
@@ -5,13 +5,18 @@ type Props = {}
 
 type State = {
   isEnter: boolean
+  offerState: string | undefined,
+  answerState: string | undefined,
 }
 
 export class Video extends React.Component<Props, State> {
   private socket: SocketIOClient.Socket;
+  // TODO: Initialize 周りのエラーが出てるので直す
   private peerConnection: RTCPeerConnection;
   private localStream: MediaStream;
+  private remoteStream: MediaStream;
   private localVideoRef: RefObject<HTMLVideoElement>;
+  private remoteVideoRef: RefObject<HTMLVideoElement>;
 
   private mediaStreamConstraints: MediaStreamConstraints = {
     audio: true,
@@ -27,30 +32,54 @@ export class Video extends React.Component<Props, State> {
     super(props)
     this.state = {
       isEnter: false,
+      offerState: undefined,
+      answerState: undefined,
     }
 
+    // 画面出力用のRef
     this.localVideoRef = React.createRef<HTMLVideoElement>();
+    this.remoteVideoRef = React.createRef<HTMLVideoElement>();
 
+    // socket.io を使ったシグナリングサーバーとの通信設定
     this.socket = io.connect('http://localhost:3001');
     this.socket.on('RECEIVE_OFFER', this.onRecieveOffer);
     this.socket.on('RECEIVE_ANSWER', this.onRecieveAnswer);
+    this.socket.on('RECEIVE_ICE', this.onReceiveIce);
   }
   
   enterRoom = async () => {
     console.log('enterRoom!')
 
+    // 入室したら自分のビデオを自分の画面に映す
     this.localStream = await navigator.mediaDevices.getUserMedia(this.mediaStreamConstraints)
     if (this.localVideoRef.current) {
       this.localVideoRef.current.srcObject = this.localStream;
     }
-    console.log(this.localStream.toString());
 
+    // PeerConnection を作成
     this.peerConnection = new RTCPeerConnection();
+
+    // PeerConnection にあらかじめイベントを設定
     this.peerConnection.onicecandidate = (ev: RTCPeerConnectionIceEvent) => {
-      console.log('onicecandidate!')
+      console.log('onicecandidate!', ev.candidate)
+
+      // Ice を送信
+      const iceCandidate = ev.candidate;
+      if (iceCandidate) this.sendIce(iceCandidate);
     }
     this.peerConnection.ontrack = (ev: RTCTrackEvent) => {
       console.log('ontrack!')
+
+      // 送られてきたTrack をStream に加工
+      this.remoteStream = new MediaStream()
+      this.remoteStream.addTrack(ev.track)
+
+      // 相手のビデオを相手の画面に映す
+      if (ev.track.kind === 'video') {
+        if (this.remoteVideoRef.current) {
+          this.remoteVideoRef.current.srcObject = this.remoteStream
+        }
+      }
     }
 
     this.setState({
@@ -60,52 +89,80 @@ export class Video extends React.Component<Props, State> {
 
   callVideo = async () => {
     console.log('callVideo!')
-    
-    const offer = await this.peerConnection.createOffer(this.offerOptions);
-    // // console.log(offer);
-    // setOfferState(offer);
 
+    // ローカルのビデオトラックをPeerConnection に追加する（この時点ではまだ相手に映像は見えない）
+    const videoTracks = this.localStream.getVideoTracks();
+    this.peerConnection.addTrack(videoTracks[0], this.localStream);
+    
+    // Offer を作成してローカルにセット (PreOffer)
+    const offer = await this.peerConnection.createOffer(this.offerOptions);
+    this.setState({
+      offerState: offer.sdp,
+    })
+    this.peerConnection.setLocalDescription(offer);
+
+    // Offer を送信する
     console.log('SEND_OFFER!')
     this.socket.emit('SEND_OFFER', { offer });
   }
-
-  // socket.once('RECEIVE_OFFER', 
   
   onRecieveOffer = async ({ id, offer }: { id: string, offer: RTCSessionDescriptionInit }) => {
     console.log('RECEIVE_OFFER! id=', id);
 
-    // RemoteにOfferをセット
+    // Offer を受け取ってリモートにセット
     const receivedOffer = new RTCSessionDescription(offer);
     console.log(receivedOffer)
+    this.setState({
+      offerState: receivedOffer.sdp,
+    })
     await this.peerConnection.setRemoteDescription(receivedOffer);
 
-    // Answerを作って投げつける
+    // Answer を作成してローカルにセット (PreAnswer)
     const answer = await this.peerConnection.createAnswer(this.offerOptions);
-    // console.log(answer);
-    // setAnswerState(answer);
-
     console.log('SEND_ANSWER!')
+    this.setState({
+      answerState: answer.sdp,
+    })
+    await this.peerConnection.setLocalDescription(answer);
+
+    // Answer を返す
     this.socket.emit('SEND_ANSWER', { id, answer });
   };
 
   onRecieveAnswer = async ({ id, answer }: { id: string, answer: RTCSessionDescriptionInit }) => {
     console.log('RECEIVE_ANSWER! id=', id)
 
-    // RemoteにAnswerをセット
+    // Answer を受け取ってリモートにセット
     const receivedAnswer = new RTCSessionDescription(answer)
     console.log(receivedAnswer)
+    this.setState({
+      answerState: receivedAnswer.sdp,
+    })
     await this.peerConnection.setRemoteDescription(receivedAnswer);
-    console.log('ALL OK!')
   };
 
+  sendIce = (iceCandidate: RTCIceCandidate) => {
+    console.log('sendIce!');
+
+    // Ice を送信する
+    this.socket.emit('SEND_ICE', { ice: iceCandidate })
+  }
+
+  onReceiveIce = async ({ ice }: { ice: RTCIceCandidate }) => {
+    console.log('onReceiveIce!');
+
+    // Ice を受け取ったらPeerConnection に追加する
+    await this.peerConnection.addIceCandidate(ice);
+  }
+
   render() {
-    const { isEnter } = this.state;
+    const { isEnter, offerState, answerState } = this.state;
     return (
       <>
         <div>
           <button style={{ width: '100px', height: '50px' }} onClick={this.enterRoom}>Enter</button>
           <button style={{ width: '100px', height: '50px' }} onClick={this.callVideo} disabled={!isEnter}>Call</button>
-          <div>{ isEnter.toString() }</div>
+          <div>{ isEnter ? '入室中' : '未入室' }</div>
           <div style={{ width: '100%', display: 'inline-flex', 'justifyContent': 'center', 'alignItems': 'center' }}>
             <div style={{ width: '50%' }}>
               <h2>You</h2>
@@ -124,27 +181,22 @@ export class Video extends React.Component<Props, State> {
               <div>
                 <video
                   style={{ marginLeft: '10%', width: '80%' }}
-                  // ref={remoteVideoRef}
+                  ref={this.remoteVideoRef}
                   playsInline
                   autoPlay
-                />
-                <audio
-                  style={{ display: 'none' }}
-                  // ref={remoteAudioRef}
-                  controls
-                  autoPlay
+                  muted
                 />
               </div>
             </div>
           </div>
-          {/* <div>
-          Offer: 
-            <input type="text" style={{ width: '100%', height: '200px' }} value={offerState?.sdp} />
+          <div>
+            <h4>Offer</h4>
+            <div style={{ width: '100%', height: '1000px', fontSize: '8px' }}>{offerState}</div>
           </div>
           <div>
-          Answer: 
-            <input type="text" style={{ width: '100%', height: '200px' }} value={answerState?.sdp} />
-          </div> */}
+            <h4>Answer</h4>
+            <div style={{ width: '100%', height: '1000px', fontSize: '8px' }}>{answerState}</div>
+          </div>
         </div>
       </>
     )
--- server/src/index.ts
+++ server/src/index.ts
@@ -15,11 +15,6 @@ app.use((_req: express.Request, res: express.Response, next: express.NextFunctio
   next();
 })
 
-
-// app.get('/', (_req: express.Request, res: express.Response) => {
-//   res.send(JSON.stringify('Hello World!'))
-// })
-
 const server = app.listen(portServer, () => {
   console.log(`Start server on port ${portServer}.`)
 })
@@ -30,58 +25,21 @@ const io = new Server(server, {
   }
 });
 
-// const roomName = 'myRoom';
 io.on('connection', socket => {
   console.log('socket.id:' + socket.id);
 
   socket.on('SEND_OFFER', ({ offer }: { offer: RTCSessionDescriptionInit }) => {
     console.log('SEND_OFFER!', socket.id);
     socket.broadcast.emit('RECEIVE_OFFER', { id: socket.id, offer });
-    // socket.emit('RECEIVE_OFFER', { offer });
   })
 
   socket.on('SEND_ANSWER', ({ id, answer }: { id: string, answer: RTCSessionDescriptionInit }) => {
     console.log('SEND_ANSWER!', socket.id);
     socket.broadcast.emit('RECEIVE_ANSWER', { id, answer });
-    // socket.emit('RECEIVE_ANSWER', { answer });
   })
 
-  // socket.on('SEND_ENTER', (_data: { roomName: string }) => {
-  //   // const { roomName } = data;
-  //   console.log('SEND_ENTER!', roomName);
-  //   socket.join(roomName);
-  //   // console.log('roomName is ', socket.rooms.values().next().value)
-  //   // console.log('rooms', socket.rooms)
-  //   socket
-  //     .broadcast
-  //     .to(roomName)
-  //     .emit('RECEIVE_CALL', { id: socket.id })
-  // })
-  
-  // socket.on('SEND_CALL', () => {
-  //   console.log('SEND_CALL!');
-  //   console.log(socket.rooms.values())
-  //   socket
-  //     .broadcast
-  //     .to(roomName)
-  //     .emit('RECEIVE_CALL', { id: socket.id })
-  // })
-
-  // socket.on('SEND_CANDIDATE', (data: { id: string, ice: RTCIceCandidate }) => {
-  //   console.log('SEND_CANDIDATE! id=', data.id, ', ice=', data.ice);
-  //   const { id, ice } = data;
-  //   socket
-  //     .broadcast
-  //     .to(roomName)
-  //     .emit('RECEIVE_CANDIDATE', { id, ice });
-  // })
-
-  // socket.on('SEND_SDP', (data: { id: string, sdp: RTCSessionDescription }) => {
-  //   const { id, sdp } = data;
-  //   console.log('SEND_SDP! id=', data.id, 'sdp.type', sdp.type);
-  //   socket
-  //     .broadcast
-  //     .to(roomName)
-  //     .emit('RECEIVE_SDP', { id, sdp });
-  // })
+  socket.on('SNED_ICE', ({ ice }: { ice: RTCIceCandidate }) => {
+    console.log('SEND_ICE!')
+    socket.broadcast.emit('RECEIVE_ICE', { ice });
+  })
 })
